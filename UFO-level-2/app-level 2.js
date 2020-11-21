5668964b0e54fa50a3e9f6eb83d38f99dbec2fd4
Eddie <eddie.xu.nyc@gmail.com>
2020-11-20 20:54:12 -0500

    created multiple filters

--- UFO-level-2/app-level 2.js
+++ UFO-level-2/app-level 2.js
@@ -6,10 +6,17 @@ var tbody = d3.select("tbody");
 var button = d3.select("#filter-btn");
 var form = d3.select("#filters");
 
+var date = d3.select("#datetime");
+var city = d3.select("#city");
+var state = d3.select("#state");
+var country = d3.select("#country");
+var shape = d3.select("#shape");
+
 //button.on("click", runEnter);
 form.on("change",runEnter);
 
 function printTable(tData){
+    tbody.html("");
     tData.forEach(function(ufoData) {
         var row = tbody.append("tr");
         Object.entries(ufoData).forEach(function([key, value]) {
@@ -23,27 +30,75 @@ printTable(tableData);
 
 function runEnter(){
     d3.event.preventDefault();
-    var filterElement = {};
 
-    var inputElement = d3.select(this).select("#input");
-    var elementValue = inputElement.property("value");
-    var filterID = inputElement.attr("id");
-    if (elementValue){
-        filterElement[filterID] = elementValue;
-        console.log(filterElement);
-    }
-    else {
-        delete filterElement[filterID];
-    }
-    
-    tbody.html("");
-    var filterData = tableData;
+    var dateValue = date.property("value");
+    var cityValue = city.property("value");
+    var stateValue = state.property("value");
+    var countryValue = country.property("value");
+    var shapeValue = shape.property("value");
 
-    Object.entries(filterElement).forEach(function([key, value]){
-        filterData = filterData.filter(row => row[key] === value);
+    var filterData = "";
+
+    if (dateValue){
+        console.log(dateValue);
+        tbody.html("");
+
+        filterData = tableData.filter(ufoData => ufoData.datetime === dateValue);
         console.log(filterData);
-    });
+        printTable(filterData);      
+    }
+    else if (cityValue){
+        console.log(cityValue);
+        tbody.html("");
+
+        filterData = tableData.filter(ufoData => ufoData.city === cityValue);
+        console.log(filterData);
+        printTable(filterData);
+    }
+    else if(stateValue){
+        console.log(stateValue);
+        tbody.html("");
+
+        filterData = tableData.filter(ufoData => ufoData.state === stateValue);
+        printTable(filterData);
+    }
+    else if(countryValue){
+        console.log(countryValue);
+        tbody.html("");
+
+        filterData = tableData.filter(ufoData => ufoData.country === countryValue);
+        printTable(filterData);
+    }
+    else if(shapeValue){
+        console.log(shapeValue);
+        tbody.html("");
+
+        filterData = tableData.filter(ufoData => ufoData.shape === shapeValue);
+        printTable(filterData);
+    }
+
+    // d3.event.preventDefault();
+    // var filterElement = {};
+
+    // var inputElement = d3.select(this).select("input");
+    // var elementValue = inputElement.property("value");
+    // var filterID = inputElement.attr("id");
+    // if (elementValue){
+    //     filterElement[filterID] = elementValue;
+    //     console.log(filterElement);
+    // }
+    // else {
+    //     delete filterElement[filterID];
+    // }
     
-    printTable(filterData);
+    // tbody.html("");
+    // var filterData = tableData;
+
+    // Object.entries(filterElement).forEach(function([key, value]){
+    //     filterData = filterData.filter(row => row[key] === value);
+    //     console.log(filterData);
+    // });
+    
+    // printTable(filterData);
 };
 
